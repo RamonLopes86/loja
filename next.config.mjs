2f4dd87445f78f3cce4efa95e85fad0196b239c0
Ramon Lopes <dev.ramondasilvalopes@gmail.com>
2024-11-02 21:46:06 -0300

    aqui

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,54 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  images:{
+
+    domains:[
+      
+      'm.media-amazon.com',
+      'logodownload.org',
+      'logosmarcas.net',
+      'http2.mlstatic.com',
+      'www.imcbrasil.com',
+      'raichu-uploads.s3.amazonaws.com',
+      'img.irroba.com.br',
+      'www.fortlev.com.br',
+      'cdn.leroymerlin.com.br',
+      'images.cws.digital',
+      'casamattos.fbitsstatic.net',
+      'img.lojadomecanico.com.br',
+      'img.lojadomecanico.com.br',
+      'static.construlardistribuidora.com.br',
+      'images.tcdn.com.br',
+      'images.tcdn.com.br',
+      'casaegaragem.fbitsstatic.net',
+      'decorpecas.fbitsstatic.net',
+      'www.saravati.com.br',
+      'd2l6qh3bt135o5.cloudfront.net',
+      'acdn.mitiendanube.com',
+      'cinquentamaissaude.vteximg.com.br',
+      'outletdastintas.com.br',
+      'global.cdn.magazord.com.br',
+      'shoppr.com.br',
+      'leaoferramentas.agilecdn.com.br',
+      'static.felap.com.br',
+      't74920.vteximg.com.br',
+      'd365e82sgxmduv.cloudfront.net'
+
+      
+
+
+
+      
+
+
+    ],
+  
+
+    
+    
+  }
 };
 
 export default nextConfig;
